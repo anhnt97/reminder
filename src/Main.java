9bcc7065d3b2b0403c8edf660b8fa3e04551bb4f
tuananh <tuananh05031997@gmail.com>
2016-11-13 22:33:35 +0700

    reminder

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by tuana on 13/11/2016.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("");
+    }
+}
---
+++ src/com/uet/reminder/GUI.java
@@ -0,0 +1,13 @@
+package com.uet.reminder;
+
+import javax.swing.*;
+
+/**
+ * Created by tuana on 13/11/2016.
+ */
+public class GUI extends JFrame{
+    public GUI(){
+        super("REMINDER");
+
+    }
+}
