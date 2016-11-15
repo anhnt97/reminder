81c2db62baa031b82caeb35448176ef63ab618ad
tuananh <tuananh05031997@gmail.com>
2016-11-16 00:57:58 +0700

    update item,logo

--- src/com/uet/reminder/Panel/DisplayPanel.java
+++ src/com/uet/reminder/Panel/DisplayPanel.java
@@ -2,8 +2,12 @@ package com.uet.reminder.Panel;
 
 import com.uet.reminder.common.CommonVLs;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Panel hien thi khi khoi dong chuong trinh
@@ -17,20 +21,18 @@ public class DisplayPanel extends JPanel{
     /** Button tim kiem */
     JButton search;
     public DisplayPanel(){
+        this.setLayout(null);
         this.setBounds(0,0, CommonVLs.WIDTH_SCREEN,CommonVLs.HEGHT_SCREEN);
         initComponents();
     }
 
     private void initComponents() {
-        logo = new ImageIcon(getClass().getResource("/image/reminder_logo.png")).getImage();
-        logo.getScaledInstance(10,10,Image.SCALE_SMOOTH);
         lbLogo = new JLabel();
-        lbLogo.setBounds(1,1,10,10);
-        lbLogo.setIcon(new ImageIcon(logo));
+        lbLogo.setBounds(10,10,200,50);
+        CommonVLs.setPicture(lbLogo,getClass().getResource("/image/reminder_logo.png").getPath());
         this.add(lbLogo);
-
-        search = new JButton("Search");
-        search.setBounds(20,30,10,50);
-        this.add(search);
+//        search = new JButton("Search");
+//        search.setBounds(20,30,100,50);
+//        this.add(search);
     }
 }
--- src/com/uet/reminder/common/CommonVLs.java
+++ src/com/uet/reminder/common/CommonVLs.java
@@ -1,5 +1,12 @@
 package com.uet.reminder.common;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 /**
  *Cac du lieu dung chung trong toan bo chuong trinh
  * Created by tuan anh on 14/11/2016.
@@ -7,4 +14,27 @@ package com.uet.reminder.common;
 public class CommonVLs {
    public static final int WIDTH_SCREEN = 700;
    public static final int HEGHT_SCREEN = 500;
+   public static void setPicture(JLabel label, String fileName) {
+      try {
+         BufferedImage image = ImageIO.read(new File(fileName));
+         int w = label.getSize().width;
+         int h = label.getSize().height;
+         int iw = image.getWidth();
+         int ih = image.getHeight();
+         int dw = 0;
+         int dh = 0;
+         if (w / h > iw / ih) {
+            dh = h;
+            dw = dh * iw / ih;
+         } else {
+            dw = w;
+            dh = dw * ih / iw;
+         }
+         ImageIcon icon = new ImageIcon(image.getScaledInstance(dw, dh,
+                 Image.SCALE_SMOOTH));
+         label.setIcon(icon);
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+   }
 }
